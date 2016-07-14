324c6fb2c2dbdf3e2c120db8bb06b8688672ae86
Chinyere Ukomadu <CUkomadu@Chinyeres-MacBook-Pro.local>
2016-07-14 18:32:04 -0500

    Time Machine Working still needs CSS Styling

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,15 +1,143 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import Backbone from 'backbone'
+
+
+
 
 const app = function() {
 
-	const Header = React.createClass({
-		render: () => {
-			return <h1>YOLO</h1>
+	let pastIntervalRef
+	let futureIntervalRef
+
+	const getCurrentYear = function(){
+		var currentDate = new Date(),
+		    currentYear = currentDate.getFullYear()
+			//console.log(currentYear)
+		return currentYear
+	}
+
+	const AppView = React.createClass({
+
+		getInitialState: function(){
+			return {
+				currentYear: getCurrentYear(),
+				clickedButton: '',
+				background: 'none'
+			}
+		},
+
+		componentWillMount: function(){
+			//console.log('this is currentYear on State':, this.state.currentYear)
+			var currentYearCopy = this.state.currentYear
+			var self = this
+			var decreaseYear = function(){
+				//var currentYearCopy = this.state.currentYear,
+					currentYearCopy -= 1
+					
+					self.setState({
+						currentYear: currentYearCopy
+					})
+			}
+
+			var increaseYear = function(){
+				//var currentYearCopy = this.state.currentYear,
+					currentYearCopy += 1
+					self.setState({
+						currentYear: currentYearCopy
+					})
+			}
+
+			var freezeYear = function(){
+				console.log('I stopped')
+				//var currentYearCopy = this.state.currentYear
+				self.setState({
+					clickedButton: currentYearCopy
+				})
+				
+			}
+
+			Backbone.Events.on('button-clicked', (clickedButton) => {
+				console.log('this is backbone receiving clickedButton:', clickedButton)
+
+				clearInterval(pastIntervalRef)
+				clearInterval(futureIntervalRef)
+
+				if(clickedButton === 'past'){
+					pastIntervalRef = setInterval(decreaseYear, 500)
+					this.setState({
+						background: 'url(https://s19.postimg.org/f0aaz6577/Plan.jpg)'
+					})
+					
+				} 
+				
+				else if(clickedButton === 'future'){
+					futureIntervalRef = setInterval(increaseYear, 500)
+					this.setState({
+						background: 'url(https://s19.postimg.org/bo64n1aab/i_Stock_80377159_XLARGE.jpg)'
+					})
+				}
+
+				else if(clickedButton === 'stop'){
+					console.log('I stopped')
+					var currentYearCopy = this.state.currentYear
+						this.setState({
+							clickedButton: currentYearCopy
+						})
+					
+				}
+			})
+		},
+
+
+		render:function(){
+			var styleObj = {
+				background: this.state.background,
+				backgroundSize: 'cover'
+			}
+			console.log(this.state.background	)
+			return (
+					<div style={styleObj} className="AppView">
+						<h1>TIME MACHINE</h1>
+						<TimeView currYear={this.state.currentYear}/>
+						<ButtonView />
+					</div>
+				)
 		}
 	})
 
-	ReactDOM.render(<Header/>,document.querySelector('.container'))
+	const TimeView = React.createClass({
+		render:function(){
+			return (
+					<div className="TimeBox">
+						<h3>Your Current Year In Time Is: <span>{this.props.currYear}</span></h3>
+					</div>
+				)
+		}
+	})
+
+	const ButtonView = React.createClass({
+
+		_getClickedButton: function(e){
+			console.log(e.target.dataset['button'])
+			var clickedButton = e.target.dataset['button']
+
+			Backbone.Events.trigger('button-clicked', clickedButton)
+		},
+
+
+		render:function(){
+			return (
+					<div className="ButtonBox" onClick={this._getClickedButton}>
+						<button data-button="past">BACK TO THE PAST</button>
+						<button data-button="stop">FREEZE TIME</button>
+						<button data-button="future">LEAP TO THE FUTURE</button>
+					</div>
+				)
+		}
+	})
+		
+	ReactDOM.render(<AppView />, document.querySelector('.container'))
 }
 
 app()
